01dc9557fe05af47a544b068882045e333361d61
Ravi Kumar <mailmeonravikumar@gmail.com>
2024-03-30 16:24:55 +0530

    Add factorial function.

---
+++ script2.js
@@ -0,0 +1,11 @@
+const factorial = (n) => {
+  let fact = 1;
+  for (i = 2; i <= n; i++) {
+    fact = fact * i;
+  }
+  return fact;
+};
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(5));
