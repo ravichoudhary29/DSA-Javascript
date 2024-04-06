b05ba47d69eca42064e64972edb4e62b86ebf088
Ravi Kumar <mailmeonravikumar@gmail.com>
2024-04-06 16:33:48 +0530

    Add binary search function.

---
+++ script8.js
@@ -0,0 +1,24 @@
+// [-5, 2, 4, 6, 10] target = 10, index = 4, target = -5, index = 0;
+
+// Solution from Tutorial
+
+const binarySearch = (arr, target) => {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (arr[middleIndex] === target) {
+      return middleIndex;
+    }
+    if (target > arr[middleIndex]) {
+      leftIndex = middleIndex + 1;
+    } else {
+      rightIndex = middleIndex - 1;
+    }
+  }
+  return -1;
+};
+
+console.log(binarySearch([-5, 2, 4, 6, 10], 10));
+console.log(binarySearch([-5, 2, 4, 6, 10], -5));
+console.log(binarySearch([-5, 2, 4, 6, 10], 6));
